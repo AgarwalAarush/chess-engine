048003a94ff91d9021a93f27be9568454002aa61
Aarush Agarwal <aarushagarwal@Aarushs-MacBook-Pro.local>
2023-06-21 21:51:34 -0700

    Chess formatting

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 Process:
 - CNN: use stockfish & generated games to determine the validity of each position
 - Minimax: use a certain deptch to acertain the best move at each position
-
-Language: C++ vs Python
--- engine.py
+++ engine.py
@@ -23,3 +23,50 @@ def stockfish_score(board, depth):
     stockfish.set_position([move.uci() for move in list(board.move_stack)])
     stockfish.set_depth(depth)
     return stockfish.get_evaluation()['value']
+
+square_index = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8
+}
+
+def square_to_index(square):
+    letter = chess.square_name(square)
+    return square_index[letter[0]] - 1, letter[1]
+
+# Piece Charts & Attack Charts
+# Experiment with adding pins, checks, threats, etc.
+def board_numerical_representation(board):
+    defined_board = numpy.zeros((14, 8, 8), dtype=numpy.int8)
+    for piece in chess.PIECE_TYPES:
+        # WHITE
+        for square in board.pieces(piece, chess.WHITE):
+            idx = numpy.unravel_index(square, (8, 8))
+            defined_board[piece - 1][idx[0]][idx[1]] = 1
+            # defined_board[12][idx[0]][idx[1]] = 1
+        # BLACK
+        for square in board.pieces(piece, chess.BLACK):
+            idx = numpy.unravel_index(square, (8, 8))
+            defined_board[piece + 5][idx[0]][idx[1]] = 1
+            # defined_board[13][idx[0]][idx[1]] = 1
+
+    # WHITE
+    board.turn = chess.WHITE;
+    legal_moves = board.legal_moves
+    for move in legal_moves:
+        idx = square_index[move.uci()[2]] - 1, int(move.uci()[3]) - 1
+        defined_board[12][idx[1]][idx[0]] = 1
+    
+    # BLACK
+    board.turn = chess.BLACK
+    legal_moves = board.legal_moves
+    for move in legal_moves:
+        idx = square_index[move.uci()[2]] - 1, int(move.uci()[3]) - 1
+        defined_board[13][idx[1]][idx[0]] = 1
+
+    return defined_board;
